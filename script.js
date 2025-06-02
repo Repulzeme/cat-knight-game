fec6c7c2a095854dbaaa5300940125f3000e15c8
Repulzeme <Najels92@hotmail.com>
2025-06-02 07:57:18 +0000

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function () {
-  const root = document.getElementById("game-root");
-  if (root) {
-    root.innerHTML = "<p>✅ Script loaded correctly!</p>";
-  }
-});
