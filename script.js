6faa897b6de35617763423a4ec6567aedbc2d78a
Repulzeme <Najels92@hotmail.com>
2025-06-02 07:41:23 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+  const root = document.getElementById("game-root");
+  if (root) {
+    root.innerHTML = "<p>✅ Script loaded correctly!</p>";
+  }
+});
